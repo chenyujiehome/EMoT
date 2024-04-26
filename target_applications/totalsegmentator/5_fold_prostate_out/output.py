f711a258fda20cef0ea28001186a41f4a8d82660
chen <2769577746@qq.com>
2024-04-26 14:17:48 +0000

    update output of prostate

--- target_applications/totalsegmentator/5_fold_prostate_out/output.py
+++ target_applications/totalsegmentator/5_fold_prostate_out/output.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd 
 dir="/home/azureuser/SuPreM/target_applications/totalsegmentator/5_fold_prostate_out"
 dirs = os.listdir(dir)
+dices=[]
+nsds=[]
 for d in dirs:
     model=d.split(".")[1]
     if os.path.isdir(os.path.join(dir,d)):
@@ -29,6 +31,64 @@ for d in dirs:
 
         # Append mean and standard deviation to the DataFrame
         nsd = pd.concat([nsd, nsd_mean.to_frame().T, nsd_std.to_frame().T])
-        dice.to_csv(f"{model}_dice.csv")
-        nsd.to_csv(f"{model}_nsd.csv")
-        
+        dices.append([dice.reset_index(),model])
+        nsds.append([nsd.reset_index(),model])
+results_dice = []
+
+for dice, model in dices:
+    # Select the last two rows and the specified columns
+    selected_rows = dice.loc[dice.index[-2:], ['background', 'PZ', 'TZ']]
+    
+    # Calculate mean and standard deviation
+    mean = selected_rows.loc[dice.index[-2]]
+    std = selected_rows.loc[dice.index[-1]]
+    
+    # Format the results as "mean +- std"
+    formatted_results = mean.astype(str) + " ± " + std.astype(str)
+    
+    # Add the model name
+    formatted_results['name'] = model
+    
+    # Append the results to the list
+    results_dice.append(formatted_results)
+
+# Convert the list of results to a DataFrame
+results_df_dice = pd.DataFrame(results_dice)
+
+
+results_nsd = []
+
+for nsd, model in nsds:
+    # Select the last two rows and the specified columns
+    selected_rows = nsd.loc[nsd.index[-2:], ['background', 'PZ', 'TZ']]
+    
+    # Calculate mean and standard deviation
+    mean = selected_rows.loc[nsd.index[-2]]
+    std = selected_rows.loc[nsd.index[-1]]
+    
+    # Format the results as "mean +- std"
+    formatted_results = mean.astype(str) + " ± " + std.astype(str)
+    
+    # Add the model name
+    formatted_results['name'] = model
+    
+    # Append the results to the list
+    results_nsd.append(formatted_results)
+
+# Convert the list of results to a DataFrame
+results_df_nsd = pd.DataFrame(results_nsd)
+
+
+# Drop the 'background' column and move the 'name' column to the first position
+results_df_dice = results_df_dice.drop(columns='background')
+results_df_dice = results_df_dice.reindex(['name'] + list(results_df_dice.columns[:-1]), axis=1)
+
+# Save the results to a CSV file
+results_df_dice.to_csv('dice.csv', index=False)
+
+# Drop the 'background' column and move the 'name' column to the first position
+results_df_nsd = results_df_nsd.drop(columns='background')
+results_df_nsd = results_df_nsd.reindex(['name'] + list(results_df_nsd.columns[:-1]), axis=1)
+
+# Save the results to a CSV file
+results_df_nsd.to_csv('nsd.csv', index=False)
