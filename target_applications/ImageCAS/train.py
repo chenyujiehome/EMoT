9bf853590c3004303903b27217082af845a4ff6c
chen <2769597746@qq.com>
2024-07-29 02:36:58 +0800

    a min a max

--- target_applications/ImageCAS/train.py
+++ target_applications/ImageCAS/train.py
@@ -290,8 +290,8 @@ def main():
 
     parser.add_argument('--batch_size', default=1, type=int, help='batch size')
     parser.add_argument('--num_workers', default=8, type=int, help='workers numebr for DataLoader')
-    parser.add_argument('--a_min', default=-999, type=float, help='a_min in ScaleIntensityRanged')
-    parser.add_argument('--a_max', default=610, type=float, help='a_max in ScaleIntensityRanged')
+    parser.add_argument('--a_min', default=-200, type=float, help='a_min in ScaleIntensityRanged')
+    parser.add_argument('--a_max', default=800, type=float, help='a_max in ScaleIntensityRanged')
     parser.add_argument('--b_min', default=0.0, type=float, help='b_min in ScaleIntensityRanged')
     parser.add_argument('--b_max', default=1.0, type=float, help='b_max in ScaleIntensityRanged')
     parser.add_argument('--space_x', default=1.5, type=float, help='spacing in x direction')
